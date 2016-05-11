3e5cdba431f01269c149bc5d2f1fa113fd1b790a
Sorcutt <shannon.orcutt88@gmail.com>
2016-05-11 15:28:32 -0400

    Accordion structure built
    With live text and FPO images populated. Responsive styles and
    scroll-triggering JS is what’s left.

--- js/script.js
+++ js/script.js
@@ -23,33 +23,40 @@ function stickIt() {
 
 // Accordion
 
-$('.hideContentHeader').prepend('<span class="indicator">+</span> ');
-    $('.hideContentHeader').click(function() {
-        $(this).parent().find('.hideContent').slideToggle("slow");
-    });
-    $('.hideContentHeader').click(function() {
-        $(this).toggleClass("active");
-    });
-    $('.hideContentHeader').click(function() {
-        $(this).find('.indicator').toggleClass("rotate");
+// $('.hideContentHeader').prepend('<span class="indicator">+</span> ');
+$('.hideContentHeader').prepend('<span class="indicator"><img src="images/plus-x-icon-wh.png"></span> ');
+
+$('.hideContentHeader').click(function() {
+    $(this).parent().find('.hideContent').slideToggle("slow");
 });
 
-// Smooth Scrolling
+$('.hideContentHeader').click(function() {
+    $(this).toggleClass("active");
+});
+
+$('.hideContentHeader').click(function() {
+    $(this).find('.indicator').toggleClass("rotate");
+});
+
+// Smooth Scrolling 
+// includes offset for #sticky-nav
 
 $(function() {
+
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top-44
         }, 1000);
         return false;
       }
     }
   });
 });
+
 
 
 
