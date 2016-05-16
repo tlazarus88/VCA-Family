f3d696e08e1dd4437cab6e6c8dd4d3b8af4973a7
tlazarus88 <trevor.das.lazarus@gmail.com>
2016-05-16 09:50:01 -0400

    Merge branch 'master' into article-anti-roadtrip
    # Conflicts:
    #	index.html

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 jQuery(document).ready(function($) {
 
-// Full Page 
+// Full Page
 
 vpw = $(window).width();
 vph = $(window).height();
@@ -29,8 +29,8 @@ scrollIntervalID = setInterval(stickIt, 10);
 function stickIt() {
   $("#sticky-nav").hide();
 
-  var $orgToC = $('#toc');   
-  var bottomToC = $orgToC.offset().top + $orgToC.outerHeight(true);           
+  var $orgToC = $('#toc');
+  var bottomToC = $orgToC.offset().top + $orgToC.outerHeight(true);
 
   if ($(window).scrollTop() >= (bottomToC)) {
     // scrolled past the original position; now only show the cloned, sticky element.
@@ -50,10 +50,10 @@ scrollIntervalID = setInterval(roadCirc, 10)
 function roadCirc() {
   $('#roadtrip-circle-2').css('visibility', 'hidden');
 
-  var $roadList = $('#list-roadtrip');   
+  var $roadList = $('#list-roadtrip');
   var topRoadList = $roadList.offset().top;
 
-  var $endRoadList = $('#last-roadtrip');   
+  var $endRoadList = $('#last-roadtrip');
   var bottomRoadList = $endRoadList.offset().top + $endRoadList.outerHeight(true);
 
   if ($(window).scrollTop() >= (topRoadList)) {
@@ -69,17 +69,71 @@ function roadCirc() {
 
 };
 
+// Surf n Skate Scroll Function
+scrollIntervalID = setInterval(surfScroll, 10)
+
+function surfScroll() {
+
+  var $surfList = $('#surf-list');
+  var topSurfList = $surfList.offset().top;
+
+  var bottomSurfList = $surfList.offset().top + $surfList.outerHeight(true);
+
+  if ($(window).scrollTop() >= (topSurfList)) {
+    $('#surf-list').css('overflow-y', 'scroll');
+  } else {
+    $('#surf-list').css('overflow-y', 'hidden');
+  };
+
+};
+
+scrollIntervalID = setInterval(skateScroll, 10)
+
+function skateScroll() {
+
+  var $skateList = $('#skate-list');
+  var topSkateList = $skateList.offset().top;
+
+  var bottomSkateList = $skateList.offset().top + $skateList.outerHeight(true);
+
+  if ($(window).scrollTop() >= (topSkateList)) {
+    $('#skate-list').css('overflow-y', 'scroll');
+  } else {
+    $('#skate-list').css('overflow-y', 'hidden');
+  };
+
+};
+
+// Surf n Skate Background Loop
+
+var surfBg = ['url(../images/surf-skate/surf_1.jpg', 'url(../images/surf-skate/surf_2.jpg', 'url(../images/surf-skate/surf_3.jpg'];
+
+$.each(surfBg, function(i, surfBg){
+    setTimeout(function(){$('.parallax-bg__surf').css('background-image', surfBg)}, (i+1)*1000);
+});
+// var now = 0;
+// var int = self.setInterval(changeBG(), 1000);
+// var array = ["1", "2", "3"];
+
+// function changeBG() {
+//   //array of backgrounds
+//   now = (now+1) % array.length ;
+//   $('.parallax-bg__surf').css('background-image', 'url(../images/surf-skate/surf_' + array[now] + '.jpg)');
+// };
+
+
+
+
 // Roadtrip Map
 
-mapboxgl.accessToken = 'pk.eyJ1IjoidGxhemFydXM4OCIsImEiOiJjaW81dDdyNnkwMjBndmlsemlsa3EzNnBrIn0.3NylPNCT2ZSfG4mLTwXyKw';
-  var map = new mapboxgl.Map({
-      container: 'roadtrip-circle-1',
-      style: 'mapbox://styles/mapbox/streets-v8',
-      center: [40, -123]
-      interactive: false,
+// mapboxgl.accessToken = 'pk.eyJ1IjoidGxhemFydXM4OCIsImEiOiJjaW81dDdyNnkwMjBndmlsemlsa3EzNnBrIn0.3NylPNCT2ZSfG4mLTwXyKw';
+//   var map = new mapboxgl.Map({
+//       container: 'roadtrip-circle-1',
+//       style: 'mapbox://styles/mapbox/streets-v8',
+//       center: [40, -123]
+//       interactive: false,
       
-  });
-
+//   });
 // var map;
 
 // function init_map(){
@@ -94,6 +148,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidGxhemFydXM4OCIsImEiOiJjaW81dDdyNnkwMjBndmlse
 // 		map: map,
 // 		position: new google.maps.LatLng(40.6813578,-73.97733169999998)
 // 	});
+
 //   // Alter Roadtrip Image
 
 //   scrollIntervalID = setInterval(mapZoom, 10)
@@ -129,7 +184,7 @@ $('.hideContentHeader').click(function() {
     $(this).find('.indicator').toggleClass("rotate");
 });
 
-// Smooth Scrolling 
+// Smooth Scrolling
 // includes offset for #sticky-nav
 
 $(function() {
@@ -148,7 +203,5 @@ $(function() {
   });
 });
 
-
-
-
 });
+
