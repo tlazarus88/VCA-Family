552baa1a55e60676f09d410cedb06b4f0122b7ad
tlazarus88 <trevor.das.lazarus@gmail.com>
2016-05-31 15:20:13 -0400

    Next/Previous Buttons Added

--- js/script.js
+++ js/script.js
@@ -74,6 +74,55 @@ function stickIt() {
   }
 };
 
+// Mobile Next/Previous Section
+
+$(function(){
+    
+    var pagePositon = 0,
+        sectionsSelector = 'section',
+        $scrollItems = $(sectionsSelector),
+        offsetTolorence = 30,
+        pageMaxPosition = $scrollItems.length - 1;
+    
+    //Map the sections:
+    $scrollItems.each(function(index,ele) {
+      $(ele).attr("debog",index).data("pos",index);
+    });
+
+    // Bind to scroll
+    $(window).bind('scroll',upPos);
+    
+    //Move on click:
+    $('#next-prev a').click(function(e) {
+        if ($(this).hasClass('next') && pagePositon+1 <= pageMaxPosition) {
+            pagePositon++;
+            $('html, body').stop().animate({ 
+                  scrollTop: $scrollItems.eq(pagePositon).offset().top - 45
+            }, 300);
+        }
+        if ($(this).hasClass('previous') && pagePositon-1 >= 0) {
+            pagePositon--;
+            $('html, body').stop().animate({ 
+                  scrollTop: $scrollItems.eq(pagePositon).offset().top - 45
+              }, 300);
+            return false;
+        }
+    });
+    
+    //Update position func:
+    function upPos(){
+       var fromTop = $(this).scrollTop();
+       var $cur = null;
+        $scrollItems.each(function(index,ele){
+            if ($(ele).offset().top < fromTop + offsetTolorence) $cur = $(ele);
+        });
+       if ($cur != null && pagePositon != $cur.data('pos')) {
+           pagePositon = $cur.data('pos');
+       }                   
+    }
+    
+});
+
 // Legal Header Pop-up
 
 $(function(){
